cae3d11de781e5ac08889f54131f8c39ce097fa3
Natalya Prokhorova <105797271+natajedi@users.noreply.github.com>
2023-09-17 23:09:10 +0300

    Add files via upload

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -5,7 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit{
+export class ProductsComponent implements OnInit {
+  products = [
+    { name: 'Product 1', price: 10 },
+    { name: 'Product 2', price: 20 },
+    { name: 'Product 3', price: 30 }
+  ];
 
   constructor() {}
 
